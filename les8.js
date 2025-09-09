72a9589b18819d382ac9413d47d0ea4a89f35ef8
Alexey Turdakov <alexeyturdakov@gmail.con>
2025-09-09 23:14:30 +0400

    function

--- les8.js
+++ les8.js
@@ -1,2 +1,14 @@
 "use strict";
 
+// Функции
+
+// function min(a,b) {
+//     if(a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+// console.log(min(1,1));
+
